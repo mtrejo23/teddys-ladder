77a3630766765c60c2d5f83588bf777e2740a630
mtrejo23 <mtrejo23@gmail.com>
2026-04-08 07:16:08 -0500

    added caching for fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,6 @@
 </script>
 
 <svelte:head>
-	<link rel="preload" href="/fonts/outfit-v15-latin-300.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 	<link rel="preload" href="/fonts/outfit-v15-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 	<link rel="preload" href="/fonts/outfit-v15-latin-700.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 	<link rel="preload" href="/fonts/indie-flower-v24-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
